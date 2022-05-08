13e1059aedf5a19172d62d6e052baff7fa573ab7
nihsok <legokoala@gmail.com>
2022-05-08 17:07:00 +0900

    cronの設定は手動で

--- aircraft/ads-b/watch.sh
+++ aircraft/ads-b/watch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#add */10 * * * * bash ~/sdr/aircraft/ads-b/watch.sh on your cron setting
+
 path='/tmp/dump1090-fa/'
 for i in {0..119};do
   if [[ ${path}history_$i.json -nt ${path}conv_history_$i.json ]];then
