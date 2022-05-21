8f8f0f68f0b633ea90b88186d0d663543d316c4c
nihsok <legokoala@gmail.com>
2022-05-21 23:45:59 +0900

    カウント数インタラクティブ化。色はもう少し考える

--- aircraft/ads-b/web/count.js
+++ aircraft/ads-b/web/count.js
@@ -24,15 +24,9 @@ d3.csv("./stat.csv").then(function(data){
       .axisBottom(x)
       .tickFormat(d3.timeFormat("%m/%d %H")))
       .style("font-size",15)
-    // .append("text")
-    //   .attr("fill", "black")
-    //   .attr("x", width / 2 )
-    //   .attr("y", 45)
-    //   .text('Time')
   svg.append("g")
     .call(d3
       .axisTop(x)
-    //  .tickValues(d3.range(0,10,1))
       .tickFormat(''))
   //y axis
   svg.append("g")
@@ -54,18 +48,47 @@ d3.csv("./stat.csv").then(function(data){
       .axisRight(y)
       .tickValues(d3.range(0,651,50))
       .tickFormat(''))
+  
+  svg.selectAll(null)
+    .data(d3.range(50,650,50))
+    .enter()
+    .append("line")
+    .attr("x1",x(new Date(data[0].time)))
+    .attr("y1",d=>y(d))
+    .attr("x2",x(new Date(data[data.length-1].time)))
+    .attr("y2",d=>y(d))
+    .style("stroke","black")
+    .style("opacity",0.1)
 
-  const color=['rgb(53,161,107)','rgb(255,40,0)','rgb(0,65,255)','rgb(102,51,0)','rgb(154,0,121)']
-
+  const color=['rgb(102,51,0)','rgb(255,40,0)','rgb(0,65,255)','rgb(53,161,107)','rgb(154,0,121)']
+  const legend={t:'T,U,Position',u:'U,Position',latlon:'Position (lon,lat,alt)',alt:'Altitude',little:'No info'}
+  const stack=d3.stack()
+  .keys(['t','u','latlon','alt','little'])(data).reverse()
+  console.log(stack)
   svg.selectAll("mylayers")
-    .data(d3.stack()
-      .keys(['t','u','latlon','alt','little'])(data))
+    .data(stack)
     .join("path")
     .style("fill",(d,i)=>color[i])
-    .style("opacity",0.8)
+    .style("opacity",0.3)
     .attr("d",d3.area()
       .x(d=>x(new Date(d.data.time)))
-      .y0(d=>y(d[0]))
+      .y0(d=>y(0))
       .y1(d=>y(d[1]))
     )
+    .on("mouseover",function(event,d){
+      console.log(d3.sum(d,(d)=>d[1]),d3.max(d,(d)=>d[1]),d3.mean(d,(d)=>d[1]))
+      d3.select(event.target).style("opacity",1)
+      tooltip
+        .style("visibility","visible")
+        .text(legend[d.key]+' Max='+d3.max(d,(d)=>d[1])+', Mean='+Math.round(d3.mean(d,(d)=>d[1])*10)/10)
+    })
+    .on("mousemove",function(event,d){
+      tooltip
+        .style("left", event.pageX + 15 + "px")
+        .style("top", event.pageY -20 + "px")
+    })
+    .on("mouseout",function(event){
+      d3.select(event.target).style("opacity",0.3)
+      tooltip.style("visibility","hidden")
+    })
 })
