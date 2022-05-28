83a1a97932d72391e1273ec95de3125568d7f671
nihsok <legokoala@gmail.com>
2022-05-28 14:09:04 +0900

    風速矢印いけたか

--- aircraft/ads-b/web/3dview.js
+++ aircraft/ads-b/web/3dview.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded',()=>{
   const width = window.innerWidth
   const height = 500
   const earthradius = 637.1//10km
-  const zfactor = 10
+  const zfactor = 5
 
   const canvasElement = document.querySelector('#myCanvas')
   const renderer = new THREE.WebGLRenderer({
@@ -31,26 +31,35 @@ window.addEventListener('DOMContentLoaded',()=>{
   scene.add(earth)
 
   const loader = new THREE.FileLoader().load('./data.csv',(data)=>{
-    const points=[]
+    const p=[]
     for (const row of data.split('\n')){
       const values = row.split(',')
-      if(values[0]>2){
+      if(values[0] > 2){
         const r = earthradius+values[1]/10000*zfactor
-        const phi =   (90-           values[3] )*Math.PI/180 //colatitude
+        const phi   = (90-           values[3] )*Math.PI/180 //colatitude
         const theta = (90+parseFloat(values[2]))*Math.PI/180 //+90 shift
 
-
-        if(values[0]>6){
+        if(values[0] > 6){
+          const cos =   Math.cos(values[3]*Math.PI/180)
+          const x = cos*Math.cos(values[2]*Math.PI/180)*values[5] - cos*values[4]
+          const y = cos*Math.sin(values[2]*Math.PI/180)*values[5] + cos*values[4]
+          const z =    -Math.sin(values[3]*Math.PI/180)*values[5]
+          const wind  = new THREE.Vector3().set(x,y,z)
           const arrow = new THREE.ArrowHelper(
-            new THREE.Vector3().set(0,0,1),
+            wind.normalize(),
             new THREE.Vector3().setFromSphericalCoords(r,phi,theta),
-            5,0xffffff)
+            wind.length()*5,
+            0xffffff)
           scene.add(arrow)
         }else{
-
+          p.push(new THREE.Vector3().setFromSphericalCoords(r,phi,theta))
         }
       }
     }
+    const geometry = new THREE.BufferGeometry().setFromPoints(p)
+    const material = new THREE.PointsMaterial({color:0xffffff,size:1})
+    const points = new THREE.Points(geometry,material)
+    scene.add(points)
   })
 
   tick()
