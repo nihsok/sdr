661c112be41e844310688625a4f8c431fb2f3641
nihsok <legokoala@gmail.com>
2022-10-29 21:48:01 +0900

    t2>0

--- aircraft/ads-b/dz.py
+++ aircraft/ads-b/dz.py
@@ -15,11 +15,11 @@ for line in input[1:]:
   if (t2 != 0): t2 += 273.15
   theta2 = t2 * (100000/p2)**rdcp if t2 > 0 else 0
 
-  if t1 > 0 and time2 - time1 < 40 and z2 != z1:
+  if t1 > 0 and t2 > 0 and time2 - time1 < 40 and z2 != z1:
     dpdz = (p2-p1) / (z2-z1)
     dtdz = (theta2-theta1) / (z2-z1)
     dvdz_square = ( (u2-u1)**2 + (v2-v1)**2 ) / (z2-z1)**2
 
     print (hex,z1,z2,t1,t2,p1,p2,theta1,theta2,dpdz,dtdz,dvdz_square,sep=',')
-    #N2=g/theta{1,2}*dtdz,Ri=N2{1,2}/dvdz_square,Dw/Dt=-g-p{1,2}/RT{1,2}*dpdz
+    #z=z{1,2},N2=g/theta{1,2}*dtdz,Ri=N2{1,2}/dvdz_square,Dw/Dt=-g-p{1,2}/RT{1,2}*dpdz
   z1,u1,v1,t1,p1,theta1,time1 = z2,u2,v2,t2,p2,theta2,time2
