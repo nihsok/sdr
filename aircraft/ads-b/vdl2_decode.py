8003154f29b1d327bbe5c0450f55fcfa3a49cfcf
nihsok <8760998+nihsok@users.noreply.github.com>
2023-05-27 20:46:40 +0900

    replace space, new pattern

--- aircraft/ads-b/vdl2_decode.py
+++ aircraft/ads-b/vdl2_decode.py
@@ -16,7 +16,7 @@ with open(sys.argv[1]) as f:
 						( -1 if code[ 5]=='S' else 1 ) * int(code[6:8]) + int(code[8:11])*0.1 /60*100, \
 						( -1 if code[11]=='W' else 1 ) * int(code[12:15]) + int(code[15:18])*0.1 /60*100, \
 						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
-						int(code[22:25]) + 273.15, code, sep=',')
+						int(code[22:25].replace(' ','')) + 273.15, code, sep=',')
 			elif ( result := re.findall(r"[NS]\d{5}[EW]\d{15}[- ][- \d]\d[ \d]{2}\d [ \d]\d",data['acars']['msg_text']) ): #pattern 1
 				for code in result:
 					wind_d = int(code[25:28]) * math.pi/180 #deg->radian
@@ -43,8 +43,8 @@ with open(sys.argv[1]) as f:
 						( -1 if code[0]=='S' else 1 ) * int(code[1:3]) + int(code[3:6])*0.1 /60*100, \
 						( -1 if code[6]=='W' else 1 ) * int(code[7:10]) + int(code[10:13])*0.1 /60*100, \
 						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
-						int(code[26:29]) + 273.15, code,sep=',')
-			elif ( result := re.findall(r"[NS]\d{5}[EW]\d{12}[ \d]\d{4}[- ][- \d]\d[ \d}{2}\d [ \d]{2}\d \d{5}",data['acars']['msg_text']) ): #pattern 2.1
+						int(code[26:29]) + 273.15, code, sep=',')
+			elif ( result := re.findall(r"[NS]\d{5}[EW]\d{12}[ \d]\d{4}[ -][ -\d]\d[ \d}{2}\d [ \d]{2}\d[ \d]\d{5}",data['acars']['msg_text']) ): #pattern 2.1
 				for code in result:
 					wind_d = int(code[28:31]) * math.pi/180
 					wind_s = int(code[31:34]) * 0.51444
@@ -52,17 +52,26 @@ with open(sys.argv[1]) as f:
 						( -1 if code[0]=='S' else 1 ) * int(code[1:3]) + int(code[3:6])*0.1 /60*100, \
 						( -1 if code[6]=='W' else 1 ) * int(code[7:10]) + int(code[10:13])*0.1 /60*100, \
 						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
-						int(code[25:28]) + 273.15, code,sep=',')
+						int(code[25:28]) + 273.15, code, sep=',')
 			elif ( result := re.findall(r"[NS]\d{5}[EW]\d{12}P\d{5}M\d{2}[ \d]{12}",data['acars']['msg_text']) ): #pattern 3
 				for code in result:
 					wind_d = int(code[28:31]) * math.pi/180
 					wind_s = int(code[31:34]) * 0.51444
-					print('2.1', code[20:22]+':'+code[22:24], int(code[13:19]) * 0.3048, \
+					print('3', code[20:22]+':'+code[22:24], int(code[13:19]) * 0.3048, \
 						( -1 if code[0]=='S' else 1 ) * int(code[1:3]) + int(code[3:6])*0.1 /60*100, \
 						( -1 if code[6]=='W' else 1 ) * int(code[7:10]) + int(code[10:13])*0.1 /60*100, \
 						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
-						( -1 if code[25]=='M' else 1 ) * int(code[26:28]) + 273.15, code,sep=',')
+						( -1 if code[25]=='M' else 1 ) * int(code[26:28]) + 273.15, code, sep=',')
+			elif( result := re.findall(r"T[NS][ \d]{2}\d\.\d{3}[EW][ \d]{2}\d\.\d{9}  \d{5}[ -][ \d]{7}",data['acars']['msg_text'])): #pattern 4
+				for code in result:
+					wind_d = int(code[33:36]) * math.pi/180
+					wind_s = int(code[36:38]) * 0.51444
+					print('4', code[17:19]+':'+code[19:21]+':'+code[21:23], int(code[25:29]) * 0.3048, \
+		   			(-1 if code[1]=='S' else 1 ) * float(code[2:9]), \
+						(-1 if code[9]=='W' else 1 ) * float(code[10:17]), \
+						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
+						int(code[29:33].replace(' ','')) + 273.15, code, sep=',')
 			elif ( result := re.findall(r"[NS]\d{5}[EW]\d{10}[- \dM]*$",data['acars']['msg_text']) ):
 				for code in result:
 
-					print('4','','','', '','','', code,sep=',')
+					print('5','','','', '','','', code,sep=',')
