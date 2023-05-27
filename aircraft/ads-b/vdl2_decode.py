27dcb89fba200cabaf5bcf6dababfb984fb78c37
nihsok <8760998+nihsok@users.noreply.github.com>
2023-05-27 22:31:17 +0900

    update pattern

--- aircraft/ads-b/vdl2_decode.py
+++ aircraft/ads-b/vdl2_decode.py
@@ -8,11 +8,11 @@ with open(sys.argv[1]) as f:
 	for line in f:
 		data = json.loads(line)['vdl2']['avlc']
 		if ('acars' in data and 'msg_text' in data['acars']):
-			if ( result := re.findall(r"[ \d]{2}\d00[NS]\d{5}[EW]\d{10}[- ][- \d]\d{4} [ \d]\d",data['acars']['msg_text']) ): #pattern 0
+			if ( result := re.findall(r"[ \d]{2}\d00[NS]\d{5}[EW]\d{6}[-\d]{4}[- ][- \d]\d{4} [ \d]\d",data['acars']['msg_text']) ): #pattern 0
 				for code in result:
 					wind_d = int(code[25:28]) * math.pi/180 #deg->radian
 					wind_s = int(code[28:31]) * 0.51444 #kt->m/s
-					print('0', code[18:20]+':'+code[20:22], int(code[0:5]) * 0.3048, \
+					print('0', ('' if code[18:22]=='----' else code[18:20]+':'+code[20:22]), int(code[0:5]) * 0.3048, \
 						( -1 if code[ 5]=='S' else 1 ) * int(code[6:8]) + int(code[8:11])*0.1 /60*100, \
 						( -1 if code[11]=='W' else 1 ) * int(code[12:15]) + int(code[15:18])*0.1 /60*100, \
 						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
@@ -71,7 +71,7 @@ with open(sys.argv[1]) as f:
 						(-1 if code[9]=='W' else 1 ) * float(code[10:17]), \
 						-wind_s * math.sin(wind_d), -wind_s * math.cos(wind_d), \
 						int(code[30:34].replace(' ','')) + 273.15, code, sep=',')
-			elif ( result := re.findall(r"[NS]\d{5}[EW]\d{10}[- \dM]*$",data['acars']['msg_text']) ):
+			elif ( result := re.findall(r"[\.\dNS]*[EW]\d{3}[- \.\dM]*$",data['acars']['msg_text']) ):
 				for code in result:
 
 					print('5','','','', '','','', code,sep=',')
