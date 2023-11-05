28f367937ac904f0100dbbd9f44cefb00f1f5ccb
nihsok <8760998+nihsok@users.noreply.github.com>
2023-11-05 11:49:18 +0900

    modify index of hex in watch.sh

--- aircraft/ads-b/watch.sh
+++ aircraft/ads-b/watch.sh
@@ -13,8 +13,8 @@ echo 'flag,alt,lon,lat,gs_x,gs_y,u,v,vt_x,vt_y,tas,t,mach,cas,ias,roll,p,dist,fl
 cat /tmp/dump1090-fa/history_*.csv | sort -g >> web/data.csv
 
 echo 'hex,z1,z2,t1,t2,p1,p2,theta1,theta2,dpdz,dtdz,dvdz_square' > web/dz.csv
-for hex in $(cut -d , -f 22 /tmp/dump1090-fa/history_*.csv | sort | uniq -d);do
-  grep -h $hex /tmp/dump1090-fa/history_*.csv | sort -k 22 | python3 dz.py >> web/dz.csv
+for hex in $(cut -d , -f 23 /tmp/dump1090-fa/history_*.csv | sort | uniq -d);do
+  grep -h $hex /tmp/dump1090-fa/history_*.csv | sort -k 23 | python3 dz.py >> web/dz.csv
 done
 # For offline analysis, replace ${path}/history_*.csv with web/data.csv. It uses file I/O.
 
