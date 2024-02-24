42b66e7641ea594bfe53ee4f3bf33f4cd9646dc1
nihsok <8760998+nihsok@users.noreply.github.com>
2024-02-24 10:12:31 +0900

    USER -> LONGNAME

--- aircraft/ads-b/watch.sh
+++ aircraft/ads-b/watch.sh
@@ -8,11 +8,11 @@ for i in {0..119};do
     [[ $jsonfile -ot                        watch.sh ]] && python3 reform.py $jsonfile #only after update reform.py
   fi
 done
-echo 'flag,alt,lon,lat,gs_x,gs_y,u,v,vt_x,vt_y,tas,t,mach,cas,ias,roll,p,dist,flight,squawk,category,version,hex,time' | cat - /tmp/dump1090-fa/history_*.csv | sort -g | curl http://localhost/~$USER/web/upload.php -X POST -F 'file=data.csv' -F data=@-
+echo 'flag,alt,lon,lat,gs_x,gs_y,u,v,vt_x,vt_y,tas,t,mach,cas,ias,roll,p,dist,flight,squawk,category,version,hex,time' | cat - /tmp/dump1090-fa/history_*.csv | sort -g | curl http://localhost/~$LONGNAME/web/upload.php -X POST -F 'file=data.csv' -F data=@-
 
 echo -e "hex,z1,z2,t1,t2,p1,p2,theta1,theta2,dpdz,dtdz,dvdz_square\n"$(for hex in $(cut -d , -f 23 /tmp/dump1090-fa/history_*.csv | sort | uniq -d);do
   grep -h $hex /tmp/dump1090-fa/history_*.csv | sort -k 23 | python3 dz.py
-done) | curl http://localhost/~$USER/web/upload.php -X POST -F 'file=dz.csv' -F data=@-
+done) | curl http://localhost/~$LONGNAME/web/upload.php -X POST -F 'file=dz.csv' -F data=@-
 # For offline analysis, replace ${path}/history_*.csv with web/data.csv. It uses file I/O.
 
 yyyymmdd=`date +"%Y%m%d" -d '-1 hour'`
