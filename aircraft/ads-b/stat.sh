b57b861a6efafc456b5f2d55da3fcbb368e3ae5d
nihsok <8760998+nihsok@users.noreply.github.com>
2024-02-24 11:47:02 +0900

    store /tmp/dump1090-fa/data.csv

--- aircraft/ads-b/stat.sh
+++ aircraft/ads-b/stat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #add 5 * * * * cd ~/sdr/aircraft/ads-b/web;bash ../stat.sh on your cron setting
 
-f0=`grep -c "^0," data.csv`
-f1=`grep -c "^1," data.csv`
-f3=`grep -c "^3," data.csv`
-f7=`grep -c "^7," data.csv`
-f15=`grep -c "^15," data.csv`
+f0=`grep -c "^0," /tmp/dump1090-fa/data.csv`
+f1=`grep -c "^1," /tmp/dump1090-fa/data.csv`
+f3=`grep -c "^3," /tmp/dump1090-fa/data.csv`
+f7=`grep -c "^7," /tmp/dump1090-fa/data.csv`
+f15=`grep -c "^15," /tmp/dump1090-fa/data.csv`
 
-[[ -e stat.csv ]] || echo 'time,little,alt,latlon,u,t' >stat.csv
+[[ -e stat.csv ]] || echo 'time,little,alt,latlon,u,t' > stat.csv
 
 echo `date +"%FT%H:00" -d "-1 hour"`,$f0,$f1,$f3,$f7,$f15 >> stat.csv
--- aircraft/ads-b/watch.sh
+++ aircraft/ads-b/watch.sh
@@ -8,11 +8,15 @@ for i in {0..119};do
     [[ $jsonfile -ot                        watch.sh ]] && python3 reform.py $jsonfile #only after update reform.py
   fi
 done
-echo 'flag,alt,lon,lat,gs_x,gs_y,u,v,vt_x,vt_y,tas,t,mach,cas,ias,roll,p,dist,flight,squawk,category,version,hex,time' | cat - /tmp/dump1090-fa/history_*.csv | sort -g | curl http://localhost/~$LOGNAME/web/upload.php -X POST -F 'file=data.csv' -F data=@-
+echo 'flag,alt,lon,lat,gs_x,gs_y,u,v,vt_x,vt_y,tas,t,mach,cas,ias,roll,p,dist,flight,squawk,category,version,hex,time' > /tmp/dump1090-fa/data.csv
+cat /tmp/dump1090-fa/history_*.csv | sort -g >> /tmp/dump1090-fa/data.csv
+curl -s http://localhost/~$LOGNAME/web/upload.php -X POST -F 'file=data.csv' -F data=@/tmp/dump1090-fa/data.csv
 
-echo -e "hex,z1,z2,t1,t2,p1,p2,theta1,theta2,dpdz,dtdz,dvdz_square\n"$(for hex in $(cut -d , -f 23 /tmp/dump1090-fa/history_*.csv | sort | uniq -d);do
+echo -e "hex,z1,z2,t1,t2,p1,p2,theta1,theta2,dpdz,dtdz,dvdz_square" > /tmp/dump1090-fa/dz.csv
+for hex in $(cut -d , -f 23 /tmp/dump1090-fa/history_*.csv | sort | uniq -d);do
   grep -h $hex /tmp/dump1090-fa/history_*.csv | sort -k 23 | python3 dz.py
-done) | curl http://localhost/~$LOGNAME/web/upload.php -X POST -F 'file=dz.csv' -F data=@-
+done >> /tmp/dump1090-fa/dz.csv
+curl -s http://localhost/~$LOGNAME/web/upload.php -X POST -F 'file=dz.csv' -F data=@/tmp/dump1090-fa/dz.csv
 # For offline analysis, replace ${path}/history_*.csv with web/data.csv. It uses file I/O.
 
 yyyymmdd=`date +"%Y%m%d" -d '-1 hour'`
