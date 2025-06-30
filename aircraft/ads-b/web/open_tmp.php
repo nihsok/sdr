3bddea5096143ccae2e2392113f6b361e8001287
nihsok <8760998+nihsok@users.noreply.github.com>
2025-06-29 19:29:27 -0600

    Replace aircraft directory  with submodule

--- aircraft/ads-b/web/open_tmp.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $filename = htmlspecialchars($_GET['file']);
-  header('Content-Type: text/csv');
-  header('Content-Disposition: attachment; filename='.$filename);
-  readfile( sys_get_temp_dir().'/'.$filename );
-?>
--- aircraft/ads-b/web/upload.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $temp_dir=sys_get_temp_dir();
-  move_uploaded_file( $_FILES['data']['tmp_name'] , $temp_dir.'/'.htmlspecialchars($_POST['file']) );
-?>
